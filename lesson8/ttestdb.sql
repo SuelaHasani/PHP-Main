2f4daca8a1d3f5c3e11ee3b52100e1e5b464b2a8
Hasanii1 <ssenadhasani@gmail.com>
2026-01-19 13:19:15 +0100

    commit lesson8

--- lesson8/ttestdb.sql
+++ lesson8/ttestdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2026 at 05:35 PM
+-- Generation Time: Jan 19, 2026 at 01:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,26 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `table1`
+-- Table structure for table `tabela1`
 --
 
-CREATE TABLE `table1` (
+CREATE TABLE `tabela1` (
   `id` int(30) NOT NULL,
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `age` varchar(255) NOT NULL
+  `age` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `table2`
+-- Table structure for table `tabela2`
 --
 
-CREATE TABLE `table2` (
+CREATE TABLE `tabela2` (
   `id` int(30) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `table1_id` varchar(11) NOT NULL
+  `Tabela1_id` int(30) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -51,32 +51,43 @@ CREATE TABLE `table2` (
 --
 
 --
--- Indexes for table `table1`
+-- Indexes for table `tabela1`
 --
-ALTER TABLE `table1`
+ALTER TABLE `tabela1`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `table2`
+-- Indexes for table `tabela2`
 --
-ALTER TABLE `table2`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `tabela2`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Tabela1_id` (`Tabela1_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `table1`
+-- AUTO_INCREMENT for table `tabela1`
 --
-ALTER TABLE `table1`
+ALTER TABLE `tabela1`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `table2`
+-- AUTO_INCREMENT for table `tabela2`
 --
-ALTER TABLE `table2`
+ALTER TABLE `tabela2`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tabela2`
+--
+ALTER TABLE `tabela2`
+  ADD CONSTRAINT `tabela2_ibfk_1` FOREIGN KEY (`Tabela1_id`) REFERENCES `tabela1` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
