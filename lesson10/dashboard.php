17484f81e5b6fd9f6f3b140d3c2b7e2f95b38c5a
SuelaHasani <suelahasani206@gmail.com>
2026-01-21 17:47:03 +0100

    commit lesson10

--- lesson10/dashboard.php
+++ lesson10/dashboard.php
@@ -14,5 +14,34 @@
     $getUsers->execute();
     $users=$getUsers->fetchAll();
 ?>
+
+<br><br>
+
+<table>
+    <thead>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Surname</th>
+        <th>Email</th>
+    </thead>
+    <tbody>
+        <?php
+            foreach($users as $user){
+        ?>
+
+           <tr>
+             <td><?= $user['id'] ?></td>
+             <td><?= $user['name'] ?></td>
+             <td><?= $user['surname'] ?></td>
+             <td><?= $user['email'] ?></td>
+             <td><? "<a href='delete.php?id=$user[id]'> Delete</a>| <a href='edit.php?id=$user[id]'> Update </a>"?></td>
+           </tr>
+           <?php
+           }
+           ?>
+    </tbody>
+</table>
+<a href="add.php">Add user</a>
+
 </body>
 </html>
